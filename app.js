5dea0b0b5fee8d78ac058b4e9c9afed775337a86
Ke Jia <ke.jia@metropolia.fi>
2024-09-09 15:08:38 +0300

    Step 4 and 5: commit by Ke Jia

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const morgan = require('morgan');
 const app = express();
 const tourRouter = require('./routes/tourRouter');
 const userRouter = require('./routes/userRouter');
+
+//app.use(morgan('dev'));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 
 // Middleware to parse JSON
 app.use(express.json());
@@ -9,8 +13,8 @@ app.use(express.json());
 // ROUTES
 
 // Use the tourRouter for all /tours routes
-app.use('/tours', tourRouter);
-app.use('/users', userRouter);
+app.use('/api/tours', tourRouter);
+app.use('/api/users', userRouter);
 
 
 const port = 4000;
